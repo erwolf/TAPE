1e0a30b258c2aa1aa4f26b8bfac2bc0b7938dda7
Emily Wolf <gost2143@yahoo.com>
2017-04-09 22:54:03 -0400

    change html text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-	render html: "hey, look, Emily's working ahead on rails!"
+	render html: "Look at me, having an actual website. Rails tutorial is going great, Timothy."
 	end
 end
